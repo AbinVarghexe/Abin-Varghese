62026b23fed6355d64ccd278ff923b9af10fd7c7
AbinVarghexe <abinvarghese092005@gmail.com>
2026-01-18 22:38:35 +0530

    Auto-sync after agent response

--- src/components/AboutHero.tsx
+++ src/components/AboutHero.tsx
@@ -28,19 +28,18 @@ const AboutHero = () => {
       className="relative min-h-[600px] md:min-h-[700px] lg:min-h-[800px] xl:min-h-screen flex items-center overflow-hidden"
       style={{ backgroundColor: "var(--color-bg-main)" }}
     >
-      {/* Image: Contained in main section, aligned to top, bottom clips */}
-      <div className="absolute left-1/2 -translate-x-1/2 top-0 w-[300px] md:w-[450px] lg:w-[600px] xl:w-[700px] h-full z-20 pointer-events-none">
-        <Image
-          src="/about/abin-varghese.png"
-          alt="Abin Varghese"
-          fill
-          sizes="(max-width: 768px) 300px, (max-width: 1024px) 450px, (max-width: 1280px) 600px, 700px"
-          className="object-cover object-top"
-          priority
-        />
-      </div>
-
-      <div className="container mx-auto px-4 md:px-8 lg:px-16 pt-24 pb-8 relative h-full flex flex-col justify-between">
+      <div className="container mx-auto px-4 md:px-8 lg:px-16 pt-24 pb-8 relative h-full flex flex-col justify-between overflow-hidden">
+        {/* Image: Contained in container div, aligned to top, bottom clips */}
+        <div className="absolute left-1/2 -translate-x-1/2 top-0 w-[300px] md:w-[450px] lg:w-[600px] xl:w-[700px] h-full z-20 pointer-events-none">
+          <Image
+            src="/about/abin-varghese.png"
+            alt="Abin Varghese"
+            fill
+            sizes="(max-width: 768px) 300px, (max-width: 1024px) 450px, (max-width: 1280px) 600px, 700px"
+            className="object-cover object-top"
+            priority
+          />
+        </div>
         <div className="relative flex flex-col items-center justify-center flex-grow">
           {/* Main Content Area */}
           <div className="relative w-full flex items-center justify-center min-h-[400px] md:min-h-[500px] lg:min-h-[600px]">
